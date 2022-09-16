4466af054cd8acc5b7f7d972e6f3a69e2a5df1b7
lemihai <84441371+lemihai@users.noreply.github.com>
2022-09-16 02:36:53 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -6,6 +6,7 @@ var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
 myOtherBox.object3D.rotation.x -= rotationSpeed;
+  myOtherBox.object3D.rotation.y += rotationSpeed;
 console.log(myOtherBox.object3D.rotation.x);
 }
 
