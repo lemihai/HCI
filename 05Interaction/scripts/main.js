8b1f07825b9f340a436ea7a6613c6145e0187b99
lemihai <84441371+lemihai@users.noreply.github.com>
2022-09-17 01:27:54 +0200

    Update main.js

--- 05Interaction/scripts/main.js
+++ 05Interaction/scripts/main.js
@@ -12,7 +12,7 @@ function spin(){
 setInterval(spin, 16);
 
 
-// myOtherBox.addEventListener('mouseenter', function(){
-// 	rotationSpeed = 0.01;
-// 	console.log('mouse enter');
-// });
+ myOtherBox.addEventListener('mouseenter', function(){
+	rotationSpeed = 0.01;
+	console.log('mouse enter');
+});
